113e47e631642a6ef1dd8a433529859dd91fa2fa
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-05-01 23:58:46 +0900

    메뉴 선택화면 메뉴 인덱스 넘기기 구현

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
 const port=8000;
-const serveStatic = require('serve-static'); 
-const path=require('path');
-const html=require('html');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req,res) {
     res.sendFile(__dirname + "/public/LandingPage.html")
 })
 
-app.get('api/users/choose',function(req,res){
-    res.sendFile(__dirname,"/public/ChooseMenu.html")
+app.get('/api/users/choose',function(req,res){
+    res.sendFile(__dirname+"/public/ChooseMenu.html")
+})
+
+app.post('/api/users/choose',function(req,res){
+    console.log(1111)
+    var arr=req.body.list;
+    console.log(arr);
+    res.send({"Success":true});
+    res.render('/public/WorldCup.html',{arr});
 })
 
 app.listen(port,console.log(`Server start at ${port}`));
